a70733b89f0e8d85cd5f3803e6489a1748ead8ef
Andrew Stellman <astellman@stellman-greene.com>
2019-05-12 13:01:39 -0500

    Added shared AssemblyInfo, updated PackageDistribution build

--- ExportRdf/Properties/AssemblyInfo.cs
+++ ExportRdf/Properties/AssemblyInfo.cs
@@ -2,15 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+/*
+ * NOTE: AssemblyCompany, AssemblyCopyright, AssemblyVersion, and AssemblyFileVersion
+ * are in ../PackageDistribution/SharedAssemblyInfo.cs
+ */
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExportRdf")]
 [assembly: AssemblyDescription("Export Publication Harvester data to an RDF NTriples file")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Stellman and Greene Consulting, LLC")]
 [assembly: AssemblyProduct("ExportRdf")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,14 +25,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("735e765d-1591-44b4-a6f5-f4cd240e48b7")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
---
+++ PackageDistribution/SharedAssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *                           Publication Harvester
+ *              Copyright © 2003-2019 Stellman & Greene Consulting
+ *      Developed for Joshua Zivin and Pierre Azoulay, Columbia University
+ *            http://www.stellman-greene.com/PublicationHarvester
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 2 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program (GPL.txt); if not, write to the Free Software Foundation, Inc., 51 
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Stellman & Greene Consulting LLC")]
+[assembly: AssemblyCopyright("Copyright © 2019 Stellman & Greene Consulting LLC")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
